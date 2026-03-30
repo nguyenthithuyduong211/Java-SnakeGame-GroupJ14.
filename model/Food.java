e675b540514d60cf51d1a7d4b4ddeaa353db1681
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-03-30 16:13:06 +0700

    Update Food.java

--- model/Food.java
+++ model/Food.java
@@ -1,1 +1,16 @@
+package model;
+import java.util.Random;
+public class Food extends GameObject {
+    private Random random;
 
+    public Food(int startX, int startY) {
+        super(startX, startY); // Gọi Constructor lớp cha
+        random = new Random();
+    }
+    // Tiêu chí: Sử dụng tốt Đa hình 
+    @Override
+    public void respawn(int maxWidth, int maxHeight) {
+        setX(random.nextInt(maxWidth));
+        setY(random.nextInt(maxHeight));
+    }
+}
