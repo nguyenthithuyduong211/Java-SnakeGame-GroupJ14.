4e16f6db5f8e7cc4e4bcc6279fabfd44bff36cae
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-03-30 16:15:18 +0700

    Update InvalidScoreException.java

--- utils/InvalidScoreException.java
+++ utils/InvalidScoreException.java
@@ -1,1 +1,7 @@
-
+package utils;
+// Tiêu chí: Tự định nghĩa ít nhất 1 Custom Exception 
+public class InvalidScoreException extends Exception {
+    public InvalidScoreException(String message) {
+        super(message);
+    }
+}
