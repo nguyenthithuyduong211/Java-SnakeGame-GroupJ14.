a22d61dfa90aeb41beeee3346c200aa2a4182d7a
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-03-30 16:14:15 +0700

    Update Ostacle.java

--- model/Ostacle.java
+++ model/Ostacle.java
@@ -1,1 +1,11 @@
+package model;
+public class Obstacle extends GameObject {
+    public Obstacle(int x, int y) {
+        super(x, y);
+    }
 
+    @Override
+    public void respawn(int maxWidth, int maxHeight) {
+        // Vật cản cố định, không respawn ngẫu nhiên như thức ăn
+    }
+}
