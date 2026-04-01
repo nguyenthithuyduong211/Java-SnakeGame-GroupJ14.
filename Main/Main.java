9081917565fa67c1a05d887467969a20c9ca4d1e
kzylazyy <hoactuan93@gmail.com>
2026-04-01 17:49:30 +0700

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,1 +1,15 @@
+import view.MainFrame;
+import javax.swing.SwingUtilities;
 
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                MainFrame game = new MainFrame();
+                game.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
