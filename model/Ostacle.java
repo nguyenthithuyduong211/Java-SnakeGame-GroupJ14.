b6e92793842c8ed6e890310dec8cc9fc68e39d74
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-04-01 18:19:29 +0700

    Update Ostacle.java

--- model/Ostacle.java
+++ model/Ostacle.java
@@ -1,11 +1,10 @@
 package model;
+
 public class Obstacle extends GameObject {
     public Obstacle(int x, int y) {
         super(x, y);
     }
 
     @Override
-    public void respawn(int maxWidth, int maxHeight) {
-        // Vật cản cố định, không respawn ngẫu nhiên như thức ăn
-    }
+    public void respawn(int maxWidth, int maxHeight) {} // cố định
 }
