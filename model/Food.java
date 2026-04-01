ddb9101489ec37132198318c3fcbbd2ceb8ca07d
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-04-01 18:18:28 +0700

    Update Food.java

--- model/Food.java
+++ model/Food.java
@@ -1,16 +1,36 @@
 package model;
-import java.util.Random;
-public class Food extends GameObject {
-    private Random random;
 
-    public Food(int startX, int startY) {
-        super(startX, startY); // Gọi Constructor lớp cha
-        random = new Random();
+import java.awt.Color;
+import java.util.List;
+import java.util.Random;
+
+public class Food extends GameObject {
+    private Random random = new Random();
+    private Color color = Color.RED;
+
+    public Food(int x, int y) {
+        super(x, y);
     }
-    // Tiêu chí: Sử dụng tốt Đa hình 
+
+    public Color getColor() { return color; }
+    public void setColor(Color c) { if (c != null) color = c; }
+
     @Override
-    public void respawn(int maxWidth, int maxHeight) {
-        setX(random.nextInt(maxWidth));
-        setY(random.nextInt(maxHeight));
+    public void respawn(int maxW, int maxH) {
+        setX(random.nextInt(maxW));
+        setY(random.nextInt(maxH));
+    }
+
+    public void respawn(int maxW, int maxH, List<Obstacle> obstacles) {
+        do {
+            respawn(maxW, maxH);
+        } while (collidesWithObstacles(obstacles));
+    }
+
+    private boolean collidesWithObstacles(List<Obstacle> obstacles) {
+        for (Obstacle o : obstacles) {
+            if (o.getX() == getX() && o.getY() == getY()) return true;
+        }
+        return false;
     }
 }
